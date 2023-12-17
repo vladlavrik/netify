e48630d07d7f9edf9300a3ee9571b3866c05ca81
vlad <vladlavrik@ukr.net>
2023-12-17 22:37:12 +0200

    Updated donate button to PayPal

--- src/components/app/AppRateBanner/AppRateBanner.tsx
+++ src/components/app/AppRateBanner/AppRateBanner.tsx
@@ -1,5 +1,4 @@
 import React, {FC} from 'react';
-import BmcIcon from './icons/bmc.svg';
 import ChromeStoreIcon from './icons/chrome-store.svg';
 import GithubIcon from './icons/github.svg';
 import styles from './appRateBanner.css';
@@ -24,19 +23,31 @@ export const AppRateBanner: FC = () => {
 				<ChromeStoreIcon className={styles.linkEntryIcon} />
 				Rate on Chrome WebStore
 			</a>
-
 			<p className={styles.title}>Support us</p>
 			<p className={styles.subtitle}>
 				Netify is fully free and open source. <br />
 				You can thank the author by making a small donation
 			</p>
-			<a
-				className={styles.linkEntry}
-				href='https://www.buymeacoffee.com/vladlavrik'
-				target='_blank'
-				rel='noreferrer'>
-				<BmcIcon />
-			</a>
+			{/* PayPal donation button start*/}
+			<form className={styles.donateButton} action='https://www.paypal.com/donate' method='post' target='_blank'>
+				<input type='hidden' name='hosted_button_id' value='49WGRXS8GF9PU' />
+				<input
+					type='image'
+					src='https://www.paypalobjects.com/en_US/i/btn/btn_donate_SM.gif'
+					style={{border: 0}}
+					name='submit'
+					title='PayPal - The safer, easier way to pay online!'
+					alt='Donate with PayPal button'
+				/>
+				<img
+					alt=''
+					style={{border: 0}}
+					src='https://www.paypal.com/en_UA/i/scr/pixel.gif'
+					width='1'
+					height='1'
+				/>
+			</form>
+			{/* PayPal donation button end*/}
 		</div>
 	);
 };
