c19960e80c00c021f9e9df4f605af974066bd02c
vladlavrik <vladlavrik@ukr.net>
2026-04-04 11:42:56 +0300

    Prevent creation of devtool panel in cases where tabId is not available

--- src/devtool.ts
+++ src/devtool.ts
@@ -3,12 +3,14 @@ import {SettingsMapper} from '@/services/settingsMapper';
 const settingsMapper = new SettingsMapper();
 const allowDevtoolsPanel = await settingsMapper.getValue('allowDevtoolsPanel');
 
-if (allowDevtoolsPanel) {
+// "tabId" can be undefined in some specific case like debugging a chrome side panel
+const {tabId} = chrome.devtools.inspectedWindow;
+
+if (allowDevtoolsPanel && tabId) {
 	let tabName = 'Netify';
 	if (process.env.NODE_ENV === 'development') {
 		tabName += ' (dev)';
 	}
 
-	const {tabId} = chrome.devtools.inspectedWindow;
 	chrome.devtools.panels.create(tabName, 'icons/logo-16.png', `application.html?mode=devtools&tabId=${tabId}`);
 }
